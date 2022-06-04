c15dcf30289cf7e95cd5ec33f86b47b51fc37571
Joschi <joschi.grey@posteo.de>
2022-06-04 23:22:41 +0200

    Created DataConnverter
    Can be used to compress .xml into .zz

---
+++ DataConverter/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.IO.Compression;
+
+var rootDirectory = Environment.CurrentDirectory;
+
+//Yeah yeah a hacky way to not throw everything into the root folder
+#if DEBUG
+    rootDirectory = rootDirectory.Replace(@"\bin\Debug\net6.0", "");
+#endif
+#if RELEASE
+    rootDirectory = rootDirectory.Replace(@"\bin\Release\net6.0", "");
+#endif
+
+Console.WriteLine(rootDirectory);
+
+string inputBase = Path.Combine(rootDirectory, "Input");
+if (!Directory.Exists(inputBase))
+    throw new DirectoryNotFoundException(inputBase);
+
+string outputBase = Path.Combine(rootDirectory, "Output");
+if (!Directory.Exists(outputBase))
+{
+    Console.WriteLine("Needed to create: " + outputBase);
+    Directory.CreateDirectory(outputBase);
+}
+
+
+var files=  Directory.GetFiles(inputBase);
+
+
+foreach (var file in files)
+{
+    
+    if (!File.Exists(file))
+        throw new FileNotFoundException(file);
+    using var input = File.OpenRead(file);
+    
+
+    var outputPath = Path.Join(outputBase, Path.GetFileName(file));
+    outputPath = Path.ChangeExtension(outputPath, ".zz");
+    using var output = File.Create(outputPath);
+
+    using var compressor = new DeflateStream(output, CompressionMode.Compress);
+    input.CopyTo(compressor);
+
+    compressor.Close();
+    output.Close();
+}
+
+
+
